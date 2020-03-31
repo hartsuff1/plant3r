c3171b34956f95c67939ac7e93f8ad94b224fe44
Greg Wishart <greg.wishart@corusent.com>
2020-03-31 14:51:29 -0400

    Got SignIn working

--- Plant3r/AppDelegate.swift
+++ Plant3r/AppDelegate.swift
@@ -6,32 +6,38 @@
 //  Copyright © 2020 G-Bear. All rights reserved.
 //
 
+// Google Sheets API KEY = "AIzaSyAbGCKb_jYBD307gFXI5sVcqAeaQcGUMDY"
+// Google Sheets Client ID = "577252377971-756bnoh8r0f88esbl8fpjkmcn8srra95.apps.googleusercontent.com"
+
+// The client ID can always be accessed from Credentials in APIs & Services
+// OAuth is limited to 100 sensitive scope logins until the OAuth consent screen is published. This may require a verification process that can take several days.
+
+
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+	{
 		// Override point for customization after application launch.
 		return true
 	}
 
 	// MARK: UISceneSession Lifecycle
 
-	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration
+	{
 		// Called when a new scene session is being created.
 		// Use this method to select a configuration to create the new scene with.
 		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
 	}
 
-	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>)
+	{
 		// Called when the user discards a scene session.
 		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
 		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 	}
-
-
 }
 
--- Plant3r/ViewController.swift
+++ Plant3r/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+class ViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate
+{
+	func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!)
+	{
+		print("X")
 	}
-
-
+	
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+		
+		//GIDSignIn.sharedInstance().presentingViewController = self
+	}
+	
+	@IBAction func buttonClicked(sender:UIButton)
+	{
+		GIDSignIn.sharedInstance().delegate = self
+		GIDSignIn.sharedInstance().uiDelegate = self as! GIDSignInUIDelegate
+		GIDSignIn.sharedInstance().clientID = "577252377971-756bnoh8r0f88esbl8fpjkmcn8srra95.apps.googleusercontent.com"
+		
+		GIDSignIn.sharedInstance().signIn()
+	}
 }
 
